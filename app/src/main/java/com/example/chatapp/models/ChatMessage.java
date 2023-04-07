c15e285bd3981bf605d6aa54e42a9d8b4c67aeb6
sang <hoanngsanng1902@gmail.com>
2023-04-07 11:38:23 +0700

    source code

---
+++ app/src/main/java/com/example/chatapp/models/ChatMessage.java
@@ -0,0 +1,8 @@
+package com.example.chatapp.models;
+
+import java.util.Date;
+
+public class ChatMessage {
+    public String senderId, receiverId, message, dateTime, conversionId, conversionName, conversionImage;
+    public Date dateObject;
+}
